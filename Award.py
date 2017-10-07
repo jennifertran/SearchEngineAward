7394c00518579f11a19c8e90aec0000df4a9ebc5
Jason Xu <jason-xu1996@hotmail.com>
2017-10-07 13:54:00 -0500

    Add award object file

---
+++ Award.py
@@ -0,0 +1,21 @@
+class Award:
+    'Contains all the basic info about awards'
+    count = 0
+
+    def __init__(self, number, name, type, application, aboriginal, renewable, value, due, description):
+        self.number = number
+        self.name = name
+        self.type = type
+        self.application = application
+        self.aboriginal = aboriginal
+        self.renewable = renewable
+        self.value = value
+        self.due = due
+        self.description = description
+        Award.count += 1
+
+        def displayCount(self):
+            print "There are totally %d awards" % Award.count
+
+        def displayAward(self):
+            print "Award Number: ",self.number,", Name: ",self.name,", Type: ",self.type,", Application: ",self.application,", Aboriginal: ",self.aboriginal,", Renewable: ",self.renewable,", Value: ",self.value,", Due Day: ",self.due,", description: ",self.description
