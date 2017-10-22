9574d2d7e15281b81299a1173df88248656d8bb4
Jennifer Tran <jennifer.tran101@gmail.com>
2017-10-22 14:22:24 -0500

    Implemented Form Validation via Flask

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from flask import Flask, request
-import ExtractData
-import os
-import jinja2
+import ExtractData, os, jinja2
 from flask_bower import Bower
-from Award import Award
+# from Award import Award
 
 # Makes a new file system path by joining the location of the current file to
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
@@ -11,18 +9,37 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), a
 
 app = Flask(__name__)
 
+# Flash uses cookies, we need a secret key
+app.secret_key = 'Manitoba'
+
 # @ signifies a decorator - a way to wrap a function and modifying it's behaviour
 @app.route('/', methods=['GET', 'POST'])
 def index():
     template = jinja_env.get_template('index.html')
+    error = None
+    errorMsg = ""
 
     if request.method == 'POST':
         currFaculty = request.form['faculty']
         currType = request.form['type']
-        awards = ExtractData.passResult(currFaculty, currType)
-        template = jinja_env.get_template('index.html')
 
-        return template.render(faculty=currFaculty, type=currType, awards=awards)
+        if currFaculty == '-1':
+            errorMsg += "Please choose faculty, "
+
+        if currType == '-1':
+            errorMsg += "Please choose award type, "
+
+        if errorMsg != "":
+            error = ""
+
+            # Slice the ", " at the end
+            error += errorMsg[:-2]
+
+        if error is None:
+            awards = ExtractData.passResult(currFaculty, currType)
+            return template.render(faculty=currFaculty, type=currType, awards=awards)
+        else:
+            return template.render(error=error)
 
     return template.render()
 
