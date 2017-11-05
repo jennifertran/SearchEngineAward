24ba92049ed849e962b2a6f2d1cbc8196e8fdaf6
Jason Xu <jason-xu1996@hotmail.com>
2017-11-05 15:17:31 -0600

    Fix bugs when multiple types are selected and implement multi-keywords search

--- ExtractData.py
+++ ExtractData.py
@@ -13,12 +13,13 @@ def passResult(faculty,type,keyword):
     typeList = ["Athletic Award", "Bursary", "Combined Medal and Prize", "Fellowship", "Medal", "Prize", "Scholarship", "Undergraduate Research Award"]
 
     # Get the original page
-    driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
+
 
     for currFaculty in faculty:
         for currType in type:
             if currType == "View all types":
                 for y in typeList:
+                    driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
                     Select(driver.find_element_by_xpath("//*[@id='faculty']")).select_by_visible_text(currFaculty)
                     Select(driver.find_element_by_xpath("//*[@id='type']")).select_by_visible_text(y)
                     Select(driver.find_element_by_xpath("//*[@id='awardsPerPage']")).select_by_visible_text("30")
@@ -32,6 +33,7 @@ def passResult(faculty,type,keyword):
                         pass
 
                 if currType == "Entrance Bursary":
+                    driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
                     Select(driver.find_element_by_xpath("//*[@id='level']")).select_by_visible_text("Entrance")
                     Select(driver.find_element_by_xpath("//*[@id='faculty']")).select_by_visible_text(currFaculty)
                     Select(driver.find_element_by_xpath("//*[@id='type']")).select_by_visible_text("Bursary")
@@ -46,6 +48,7 @@ def passResult(faculty,type,keyword):
                         pass
 
                 if currType == "Entrance Scholarship":
+                    driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
                     Select(driver.find_element_by_xpath("//*[@id='level']")).select_by_visible_text("Entrance")
                     Select(driver.find_element_by_xpath("//*[@id='faculty']")).select_by_visible_text(currFaculty)
                     Select(driver.find_element_by_xpath("//*[@id='type']")).select_by_visible_text("Scholarship")
@@ -60,6 +63,7 @@ def passResult(faculty,type,keyword):
                         return result
 
             if (currType != "Entrance Bursary") and (currType != "Entrance Scholarship") and (currType != "View all types"):
+                driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
                 Select(driver.find_element_by_xpath("//*[@id='faculty']")).select_by_visible_text(currFaculty)
                 Select(driver.find_element_by_xpath("//*[@id='type']")).select_by_visible_text(currType)
                 Select(driver.find_element_by_xpath("//*[@id='awardsPerPage']")).select_by_visible_text("30")
@@ -73,6 +77,7 @@ def passResult(faculty,type,keyword):
                     pass
 
             if currType == "Entrance Bursary":
+                driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
                 Select(driver.find_element_by_xpath("//*[@id='level']")).select_by_visible_text("Entrance")
                 Select(driver.find_element_by_xpath("//*[@id='faculty']")).select_by_visible_text(currFaculty)
                 Select(driver.find_element_by_xpath("//*[@id='type']")).select_by_visible_text("Bursary")
@@ -87,6 +92,7 @@ def passResult(faculty,type,keyword):
                     pass
 
             if currType == "Entrance Scholarship":
+                driver.get("https://wwwapps.cc.umanitoba.ca:8443/searchableAwards/searchForm/awardSearch")
                 Select(driver.find_element_by_xpath("//*[@id='level']")).select_by_visible_text("Entrance")
                 Select(driver.find_element_by_xpath("//*[@id='faculty']")).select_by_visible_text(currFaculty)
                 Select(driver.find_element_by_xpath("//*[@id='type']")).select_by_visible_text("Scholarship")
@@ -107,6 +113,7 @@ def extractAwards(currPage,keyword):
 
     awards = []
     result = []  # Contains keyword related result
+    keywords = [] # Collection of keyword
 
     type = soup.find(id="award").text.split( )
 
@@ -115,15 +122,41 @@ def extractAwards(currPage,keyword):
         name = award.find('a').text
         spanList = award.find_all(id="rightTag")
         #for span in spanList: print span.text
-        award = Award(url, spanList[0].text, name, type[3], spanList[1].text, spanList[2].text, None, None, None,award.find(id="awardDesc").text.strip())
+        award = Award(url, spanList[0].text, name, type[3], spanList[1].text, spanList[2].text, None, None, None,award.find(id="awardDesc").text.strip()[:-1][1:])
         awards.append(award)
 
+
+
     if keyword:
-        for award in awards:
-            awardInfo = str(award.displayAward())
-            if awardInfo.find(keyword)!=-1:
-                result.append(award)
-        return result
+        # try to split string based on logical conjunction
+        # if find logical conjunction AND
+        if keyword.find("AND")!=-1:
+            keywords = keyword.split("AND")
+            for award in awards:
+                isFound = True
+                awardInfo = str(award.displayAward())
+                for currKeyword in keywords:
+                    if awardInfo.find(currKeyword.strip())==-1:
+                        isFound = False
+                if(isFound == True):
+                    result.append(award)
+            return result
+        #if find logical conjunction OR
+        elif keyword.find("OR")!=-1:
+            keywords = keyword.split("OR")
+            for award in awards:
+                awardInfo = str(award.displayAward())
+                for currKeyword in keywords:
+                    if awardInfo.find(currKeyword.strip())!=-1:
+                        result.append(award)
+            return result
+        # no logic conjunction is found, only single keyword
+        else:
+            for award in awards:
+                awardInfo = str(award.displayAward())
+                if awardInfo.find(keyword) != -1:
+                    result.append(award)
+            return result
     else:
         return awards
 
